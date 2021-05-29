05ce5e60efa67a6c9260fe236cbfa0905e4c5a48
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-29 17:51:17 +0300

    Секция "standart"  сконфигурирована

--- AppStoreLayout/ViewController.swift
+++ AppStoreLayout/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
         
         // MARK: Регистрируем Cells
         collectionView.register(PromotedAppCollectionViewCell.self, forCellWithReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier)
+        collectionView.register(StandardAppCollectionViewCell.self, forCellWithReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier)
         
         configureDataSource()
         
@@ -47,6 +48,21 @@ class ViewController: UIViewController {
                 section.orthogonalScrollingBehavior = .groupPagingCentered
                 
                 return section
+            case .standard:
+                // MARK: Standart section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1/3))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(250))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 3)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                
+                return section
+                
+        
             default:
                 return nil
             }
@@ -63,7 +79,11 @@ class ViewController: UIViewController {
             case .promoted:
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier, for: indexPath) as! PromotedAppCollectionViewCell
                 cell.configureCell(item.app!)
-                
+                return cell
+            case .standard:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier, for: indexPath) as! StandardAppCollectionViewCell
+                let isThirdItem = (indexPath.row + 1).isMultiple(of: 3)
+                cell.configureCell(item.app!, hideBottomLine: isThirdItem)
                 return cell
             default:
                 fatalError("Not yet implemented")
@@ -72,10 +92,18 @@ class ViewController: UIViewController {
         
         // MARK: Определяем Snapshot
         var snapshot = NSDiffableDataSourceSnapshot<Section,Item>()
+        // для promoted
         snapshot.appendSections([.promoted])
         snapshot.appendItems(Item.promotedApps, toSection: .promoted)
-        sections = snapshot.sectionIdentifiers
+        // для standart
+        let popularSection = Section.standard("Popular this week")
+        let essentialSection = Section.standard("Essential Picks")
+        snapshot.appendSections([popularSection,essentialSection])
+        snapshot.appendItems(Item.popularApps, toSection: popularSection)
+        snapshot.appendItems(Item.essentialApps, toSection: essentialSection)
         
+        // общее
+        sections = snapshot.sectionIdentifiers
         dataSource.apply(snapshot)
     }
     
